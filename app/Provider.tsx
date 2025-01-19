069ac244f62a08c9e19a3a180aa555b23fdcfa1a
Igor Paes <igor.paes@nextip.com.br>
2025-01-19 20:17:26 -0300

    feat: notifications

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -9,13 +10,22 @@ import {
 import React, { ReactNode } from "react";
 
 function Provider({ children }: { children: ReactNode }) {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       resolveUsers={async ({ userIds }) => {
-        const users = await getClerkUsers(userIds);
+        const users = await getClerkUsers({ userIds });
         return users;
       }}
       authEndpoint={"/api/liveblocks-auth"}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser!.emailAddresses[0].emailAddress,
+          text
+        });
+        return roomUsers;
+      }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
